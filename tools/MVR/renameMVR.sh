cd084b6340388e8b92c9b6b794addc72c053391e
Adam Tyson <adam.tyson@icr.ac.uk>
2018-01-26 10:32:01 +0000

    added renameMVR

--- tools/MVR/renameMVR.sh
+++ tools/MVR/renameMVR.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # script to rename files exported from Slidebook so that they play nicely with MVR
-
 rename -v T00 T *.tif
 rename -v T0 T *.tif
+rename -v _T_ _T0_ *tif
 rename C1 C90 *.tif
+rename -v "Capture " Img *tif
+
